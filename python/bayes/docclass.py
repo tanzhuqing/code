ef94e797efd42f6fa8dd67a578a7a357f4a37935
tanzhuqing <tanzhuqing123@163.com>
2013-09-01 15:57:29 +0800

    fisher方法

--- python/bayes/docclass.py
+++ python/bayes/docclass.py
@@ -22,7 +22,15 @@ class classifier:
     def __init__(self,getfeatures,filename=None):
         self.fc = {}
         self.cc = {}
+        self.thresholds = {}
         self.getfeatures = getfeatures
+
+    def setthresholds(self,cat,t):
+        self.thresholds[cat] = t
+
+    def getthreshold(self,cat):
+        if cat not in self.thresholds:return 1.0
+        return self.thresholds[cat]
 
     def incf(self,f,cat):
         self.fc.setdefault(f,{})
@@ -66,3 +74,84 @@ class classifier:
         totals = sum([self.fcount(f,c) for c in self.categories()])
         bp = ((weight*ap) + (totals*basicprob))/(weight + totals)
         return bp
+
+    def classify(self,item,default = None):
+        probs = {}
+        max = 0.0
+        for cat in self.categories():
+            probs[cat] = self.prob(item,cat)
+            if probs[cat]>max:
+                max = probs[cat]
+                best = cat
+                
+        for cat in probs:
+            if cat == best:continue
+            if probs[cat]*self.getthreshold(best) > probs[best]:return default
+        return best
+
+
+class naivebayes(classifier):
+    def docprob(self,item,cat):
+        features = self.getfeatures(item)
+
+        p=1
+        for f in features:p*=self.weightedprob(f,cat,self.fprob)
+        return p
+    def prob(self,item,cat):
+        catprob = self.catcount(cat)/self.totalcount()
+        docprob = self.docprob(item,cat)
+        return docprob*catprob
+
+
+class fisherclassifier(classifier):
+    def __init__(self,getfeatures):
+        classifier.__init__(self,getfeatures)
+        self.mininums = {}
+
+    def setminimum(self,cat,min):
+        self.mininums[cat]=min
+
+    def getminimum(self,cat):
+        if cat not in self.mininums:return 0
+        return self.mininums[cat]
+    
+    def cprob(self,f,cat):
+        clf = self.fprob(f,cat)
+        if clf == 0:return 0
+
+        freqsum = sum([self.fprob(f,c) for c in self.categories()])
+
+        p = clf/(freqsum)
+
+        return p
+    
+    def fisherprob(self,item,cat):
+        p = 1
+        features = self.getfeatures(item)
+        for f in features:
+            p*=(self.weightedprob(f,cat,self.cprob))
+
+        fscore = -2*math.log(p)
+
+        return self.invchi2(fscore,len(features)*2)
+    
+    def invchi2(self,chi,df):
+        m = chi/2.0
+        sum = term = math.exp(-m)
+        for i in range(1,df//2):
+            term *= m/i
+            sum += term
+        return min(sum,1.0)
+
+    def classify(self,item,default = None): 
+        best = default
+        max = 0.0
+        for c in self.categories():
+            p = self.fisherprob(item,c)
+
+            if p > self.getminimum(c) and p > max:
+                best = c
+                max = p
+        return best
+        
+    
